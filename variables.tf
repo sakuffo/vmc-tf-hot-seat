9f160848467d4d95c11590d8f24c8961a32e745e
Stephen Akuffo <sa.akuffo@gmail.com>
2023-05-30 11:09:31 -0400

    first template

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "api_token" {
+  
+}
+
+variable "org_id" {
+  
+}
+  
+variable "aws_account_number" {
+  
+}
+
+variable "sddc_region" {
+  
+}
+
+variable "sddc_name" {
+  
+}
+
+variable "vpc_cidr" {
+  
+}
+
+variable "sddc_num_hosts" {
+  
+}
+
+variable "vxlan_subnet" {
+  
+}
+
+variable "public_ip_displayname" {
+  
+}
